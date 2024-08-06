eec24bca6931d38b0574d87df3ac560fbf2f8e73
Rutwik More <rutwikmore@Rutwiks-MacBook-Air-286.local>
2024-08-06 12:15:41 +0530

    Login signup pages

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -5,20 +5,24 @@ import { VStack } from "@chakra-ui/layout";
 import { useToast } from "@chakra-ui/toast";
 import axios from "axios";
 import { useState } from "react";
-import { useHistory } from "react-router";
+import { useHistory } from "react-router-dom";
+
+
 
 const Signup = () => {
   const [show, setShow] = useState(false);
-  const handleClick = () => setShow(!show);
-  const toast = useToast();
-  const history = useHistory();
-
   const [name, setName] = useState();
   const [email, setEmail] = useState();
   const [confirmpassword, setConfirmpassword] = useState();
   const [password, setPassword] = useState();
   const [pic, setPic] = useState();
+  const [loading, setLoading] = useState(false);
   const [picLoading, setPicLoading] = useState(false);
+  const toast = useToast();
+  const history = useHistory();
+
+const handleClick = () => setShow(!show);
+
 
   const submitHandler = async () => {
     setPicLoading(true);
@@ -101,8 +105,8 @@ const Signup = () => {
       const data = new FormData();
       data.append("file", pics);
       data.append("upload_preset", "chat-app");
-      data.append("cloud_name", "piyushproj");
-      fetch("https://api.cloudinary.com/v1_1/piyushproj/image/upload", {
+      data.append("cloud_name", "dyenzfx2w");
+      fetch("https://api.cloudinary.com/v1_1/dyenzfx2w/image/upload", {
         method: "post",
         body: data,
       })
@@ -128,6 +132,114 @@ const Signup = () => {
       return;
     }
   };
+//   const submitHandler = async () => {
+//     setPicLoading(true);
+//     if (!name || !email || !password || !confirmpassword) {
+//       toast({
+//         title: "Please Fill all the Feilds",
+//         status: "warning",
+//         duration: 5000,
+//         isClosable: true,
+//         position: "bottom",
+//       });
+//       setPicLoading(false);
+//       return;
+//     }
+//     if (password !== confirmpassword) {
+//       toast({
+//         title: "Passwords Do Not Match",
+//         status: "warning",
+//         duration: 5000,
+//         isClosable: true,
+//         position: "bottom",
+//       });
+//       return;
+//     }
+//     console.log(name, email, password, pic);
+//     try {
+//       const config = {
+//         headers: {
+//           "Content-type": "application/json",
+//         },
+//       };
+//       const { data } = await axios.post(
+//         "/api/user",
+//         {
+//           name,
+//           email,
+//           password,
+//           pic,
+//         },
+//         config
+//       );
+//       console.log(data);
+//       toast({
+//         title: "Registration Successful",
+//         status: "success",
+//         duration: 5000,
+//         isClosable: true,
+//         position: "bottom",
+//       });
+//       localStorage.setItem("userInfo", JSON.stringify(data));
+//       setPicLoading(false);
+//       history.push("/chats");
+//     } catch (error) {
+//       toast({
+//         title: "Error Occured!",
+//         description: error.response.data.message,
+//         status: "error",
+//         duration: 5000,
+//         isClosable: true,
+//         position: "bottom",
+//       });
+//       setPicLoading(false);
+//     }
+//   };
+
+//   const postDetails = (pics) => {
+//     setPicLoading(true);
+//     if (pics === undefined) {
+//       toast({
+//         title: "Please Select an Image!",
+//         status: "warning",
+//         duration: 5000,
+//         isClosable: true,
+//         position: "bottom",
+//       });
+//       return;
+//     }
+//     console.log(pics);
+//     if (pics.type === "image/jpeg" || pics.type === "image/png") {
+//       const data = new FormData();
+//       data.append("file", pics);
+//       data.append("upload_preset", "chat-app");
+//       data.append("cloud_name", "piyushproj");
+//       fetch("https://api.cloudinary.com/v1_1/piyushproj/image/upload", {
+//         method: "post",
+//         body: data,
+//       })
+//         .then((res) => res.json())
+//         .then((data) => {
+//           setPic(data.url.toString());
+//           console.log(data.url.toString());
+//           setPicLoading(false);
+//         })
+//         .catch((err) => {
+//           console.log(err);
+//           setPicLoading(false);
+//         });
+//     } else {
+//       toast({
+//         title: "Please Select an Image!",
+//         status: "warning",
+//         duration: 5000,
+//         isClosable: true,
+//         position: "bottom",
+//       });
+//       setPicLoading(false);
+//       return;
+//     }
+//   };
 
   return (
     <VStack spacing="5px">

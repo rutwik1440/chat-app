0d10b311b04cab60cbb6695c9d26f0fea5c5d348
Rutwik More <rutwikmore@Rutwiks-MacBook-Air-286.local>
2024-08-07 00:37:59 +0530

    chats endpoints

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -61,6 +61,20 @@ const authUser = asyncHandler (async (req, res) => {
         res.status(400);
         throw new Error('Invalid Email or Password');
     }
+});
+
+const allUsers = asyncHandler (async (req, res) => {
+    const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+      console.log(keyword);
+    const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+    res.send(users);
 })
 
-module.exports = { registerUser, authUser };
+module.exports = { registerUser, authUser, allUsers };
